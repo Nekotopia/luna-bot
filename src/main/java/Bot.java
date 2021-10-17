47a15baa1941f66f62f423790bd1c6aa2ce2eed3
LunaticCat <yannbrun1507@outlook.fr>
2021-10-17 15:43:44 +0200

    created basic bot structure

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,5 +1,27 @@
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
 public class Bot {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+
+    private Bot() throws LoginException {
+
+        JDABuilder.createDefault(
+                Config.get("token"),
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.GUILD_EMOJIS
+        )
+                .addEventListeners(new Listener())
+                .setActivity(Activity.watching("kittens"))
+                .build();
+
+    }
+
+    public static void main(String[] args) throws LoginException {
+        new Bot();
     }
 }
